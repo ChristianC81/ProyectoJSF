6dad75140f86613aa623c091e9c5079dcfc9fc15
ChristianC81 <christian.crespo.est@tecazuay.edu.ec>
2023-06-10 18:04:10 -0500

    Initial commit

---
+++ src/java/controlador/EmpleadoFacade.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import modelo.Empleado;
+
+/**
+ *
+ * @author chris
+ */
+@Stateless
+public class EmpleadoFacade extends AbstractFacade<Empleado> {
+
+    @PersistenceContext(unitName = "ProyectoJSFPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public EmpleadoFacade() {
+        super(Empleado.class);
+    }
+    
+}
